e7b284fdd93f850bb986f0d6aad78a33ab1d0e7b
Jthornhill70 <justinpthornhill@gmail.com>
2022-02-01 15:33:40 -0600

    Finished interaction code

---
+++ Colorful Unity/Assets/Jthor/Code/ColorChanger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour, IInteractable 
+{
+
+    Material mat;
+
+    public void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+    public string GetDescription()
+    {
+        return "Change to a random color";
+
+    }
+
+    public void Interact()
+    {
+        mat.color = new Color(Random.value, Random.value, Random.value);
+
+    }
+
+
+
+
+
+
+}
