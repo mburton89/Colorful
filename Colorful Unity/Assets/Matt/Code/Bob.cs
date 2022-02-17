a758363516429a4d19da9a208d911ea3f13e9093
WEBSTERAD\wustudent <dts@webster.edu>
2022-02-16 20:50:55 -0600

    2

--- Colorful Unity/Assets/Matt/Code/Bob.cs
+++ Colorful Unity/Assets/Matt/Code/Bob.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Bob : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float maxyPos;
+    public float minyPos;
+
     void Start()
     {
-        
+        StartCoroutine(BobCo());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator BobCo()
     {
-        
+        transform.DOMoveY(maxyPos, 1).SetEase(Ease.InOutQuad);
+        yield return new WaitForSeconds(1);
+        transform.DOMoveY(minyPos, 1).SetEase(Ease.InOutQuad);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(BobCo());
     }
 }
