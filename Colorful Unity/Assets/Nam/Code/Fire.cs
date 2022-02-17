45abd4efeb63bc0972e5c29dda718c668d22eade
WEBSTERAD\wustudent <dts@webster.edu>
2022-02-16 20:41:10 -0600

    Started Coroutine Code
    Flash

--- Colorful Unity/Assets/Nam/Code/Fire.cs
+++ Colorful Unity/Assets/Nam/Code/Fire.cs
@@ -13,5 +13,22 @@ public class Fire : MonoBehaviour
             print("Fire!");
         }
     }
+
+    private void OnTriggerExit (Collider other)
+    {
+        
+        if(other.tag == "Player")
+        {
+            StartCoroutine("FlashColor");
+            print("Change Color");
+        }
+    }
+
+    IEnumerator FlashColor()
+    {
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(0.5f);
+        print("Bruh");
+    }
 }
 
