15f6132a4bfdc73e7db463640b56284c1f7153ba
WEBSTERAD\wustudent <dts@webster.edu>
2022-03-23 21:32:35 -0500

    all

---
+++ Colorful Unity/Assets/Noah/Code/PressurePlate.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PressurePlate : MonoBehaviour
+{
+    float initialYPosition;
+    [SerializeField] float distanceToMove;
+    [SerializeField] private GameObject simonSaysHud;
+    [SerializeField] ParticleSystem particleSystem;
+    public Color simonColor;
+
+    [SerializeField] MeshRenderer meshRenderer;
+    [SerializeField] Material unlit;
+    [SerializeField] Material lit;
+
+    void Start()
+    {
+        initialYPosition = transform.position.y;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.position = new Vector3(transform.position.x, initialYPosition - distanceToMove, transform.position.z);
+        simonSaysHud.SetActive(true);
+        particleSystem.startColor = simonColor;
+        particleSystem.Play();
+        meshRenderer.material = lit;
+        FindObjectOfType<DialogueTrigger>().TriggerDialogue();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.position = new Vector3(transform.position.x, initialYPosition, transform.position.z);
+        simonSaysHud.SetActive(false);
+        particleSystem.Stop();
+        meshRenderer.material = unlit;
+    }
+}
