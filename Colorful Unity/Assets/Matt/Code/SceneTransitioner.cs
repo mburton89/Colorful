1f5396000ee674b4969ca03e5057be49fd4fc318
WEBSTERAD\WUStudent <dts@webster.edu>
2022-05-09 16:35:40 -0500

    YELLOW

---
+++ Colorful Unity/Assets/Matt/Code/SceneTransitioner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    [SerializeField] int sceneToLoad;
+    [SerializeField] int secondsToWait;
+    [SerializeField] Image fadeImage;
+
+    private void Start()
+    {
+        fadeImage.DOFade(1, 0);
+        fadeImage.DOFade(0, secondsToWait);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GoToNexLevel();
+        if (other.GetComponent<MenteBacata.ScivoloCharacterControllerDemo.SimpleCharacterController>())
+        {
+            other.GetComponent<MenteBacata.ScivoloCharacterControllerDemo.SimpleCharacterController>().gravity = 0;
+        }
+    }
+
+    private void GoToNexLevel()
+    {
+        StartCoroutine(GoToNexLevelCo());
+    }
+
+    private IEnumerator GoToNexLevelCo()
+    {
+        fadeImage.DOFade(1, secondsToWait);
+        yield return new WaitForSeconds(secondsToWait);
+        SceneManager.LoadScene(secondsToWait);
+    }
+}
